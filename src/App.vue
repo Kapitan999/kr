9244b23077b5a42e86bacf7dbe7610fd69ae0080
Kostya <Lavreckiy.ka@gmail.com>
2021-01-31 01:40:07 +0300

    эл

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="app">
+      <v-navigation-drawer
+      color="green"
+      dark
+      expand-on-hover
+      hide-overlay
+      permanent
+      right
+      app
+      >
+      </v-navigation-drawer>
+    
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
