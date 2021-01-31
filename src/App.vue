b0ab0d20f09b473376805696a3106ca58ee7c233
Kostya <Lavreckiy.ka@gmail.com>
2021-01-31 14:15:06 +0300

    э

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,60 @@
 <template>
-  <v-app id="app">
-      <v-navigation-drawer
-      color="green"
-      dark
-      expand-on-hover
-      hide-overlay
-      permanent
-      right
+  <v-app>
+    <v-app-bar
       app
-      >
-    <v-list
-    nav 
-    shaped
-    dense>
-     <v-list-item two line>
-     <v-list-item-avatar>
-     
-     </v-list-item-avatar>
-     </v-list-item>
-    </v-list>
-    
-      </v-navigation-drawer>
-    
-    <v-content class="px-12 py-3">
-      <v-container fluid>
-        <v-container/>
-      </v-container>
-    </v-content>
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
 
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
+
   components: {
     HelloWorld,
-    v-app: () => import('./app.vue'),
-    v-navigation-drawer: () => import('./app.vue'),
-    v-list: () => import('./app.vue'),
-    v-list-item: () => import('./app.vue'),
-    v-list-item-avatar: () => import('./app.vue'),
-    v-content: () => import('./app.vue'),
-    v-container: () => import('./app.vue'),
-    v-container: () => import('./app.vue')
-  }
-}
-</script>
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
