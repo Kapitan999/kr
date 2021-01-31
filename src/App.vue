90694b36f2616af08abe60a1d677907f92c078c6
Kostya <Lavreckiy.ka@gmail.com>
2021-01-31 14:42:37 +0300

    э

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,36 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
+   <v-app id="app">
+      <v-navigation-drawer
+      color="green"
       dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      expand-on-hover
+      hide-overlay
+      permanent
+      right
+      app
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <v-list
+       nav 
+      shaped
+      dense
+      >
+      <v-list-item two-line>
+       <v-list-item-avatar>
+         <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="">
+      </v-list-item>
+      <v-list-item-content class="text-left">
+        <v-list-item-title class="font-weight-black">SocialLink</v-list-item-title>
+        <v-list-item-subtitle>Иван Иванов</v-list-item-subtitle>
+      </v-list-item-content>
+      </v-list>
+      </v-navigation-drawer>
+    
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
   </v-app>
 </template>
 
