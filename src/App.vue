f1cc00f83d151d6ef32205a3e3aabcc763d18889
Kostya <Lavreckiy.ka@gmail.com>
2021-01-31 19:05:11 +0300

    э

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
- <v-app id="app"
+   <v-app id="app"
    color="grey">
       <v-navigation-drawer
       color="green"
@@ -82,8 +82,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
-
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -93,16 +92,5 @@ export default {
   margin-top: 60px;
 }
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
