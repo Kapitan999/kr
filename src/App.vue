493038e539a59482232f806850baf1d1e8ac53d6
Kostya <Lavreckiy.ka@gmail.com>
2021-01-31 15:18:50 +0300

    э

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,17 @@
       shaped
       dense
       >
-      <v-list-item two-line>
+      <v-list-item  two-line>
        <v-list-item-avatar>
          <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="">
-      </v-list-item>
+       </v-list-item-avatar>
       <v-list-item-content class="text-left">
         <v-list-item-title class="font-weight-black">SocialLink</v-list-item-title>
         <v-list-item-subtitle>Иван Иванов</v-list-item-subtitle>
       </v-list-item-content>
+      </v-list-item>
       </v-list>
+      <v-divider class="my-3"></v-divider>
       </v-navigation-drawer>
     
     <v-content class="px-12 py-3">
@@ -35,13 +37,13 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+   // HelloWorld,
   },
 
   data: () => ({
@@ -49,3 +51,14 @@ export default {
   }),
 };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
